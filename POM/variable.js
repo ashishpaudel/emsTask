e5585196dd669b5ce89b5cb7472221e0c662c677
ashishpaudel <ashishpoudel.1998@gmail.com>
2025-09-21 12:16:53 +0545

    feat : add variables for authentication test

---
+++ POM/variable.js
@@ -0,0 +1,13 @@
+export const loginData = {
+    //admin data
+    "admin_email": "admin@ems.com",
+    "admin_password": "8r1S8[yJ",
+
+    //employee data
+    "emp_email":"employee@ems.com",
+    "emp_password":"1r1S8[yG",
+
+    //invalid data
+    "invalid_email":"admin@ems",
+    "invalid_password":"8r1S8yJ"
+};
